c70df54da5615c93dc4e63551ef2da37329a93a9
Nikita Shah <nikita12.shah@gmail.com>
2024-12-04 22:33:51 +0530

    Example for a simple function in JavaScript.

---
+++ Example-Simple-Function.js
@@ -0,0 +1,21 @@
+// ====== Code ====== //
+
+function logHello() {
+    console.log("Hello from a function!");
+}
+
+logHello();
+
+
+// ==================================== //
+
+
+// ====== Output ====== //
+
+/*
+
+Hello from a function!
+
+=== Code Execution Successful ===
+
+*/
