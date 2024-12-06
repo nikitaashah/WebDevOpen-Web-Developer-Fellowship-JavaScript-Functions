486cb1ff5c1b1531cf5126636fc0c81b1367c46d
Nikita Shah <nikita12.shah@gmail.com>
2024-12-06 21:06:33 +0530

    Example to create and call a function in JavaScript.

---
+++ Create-Your-Own-Function.js
@@ -0,0 +1,25 @@
+// Declare a function called sayHello
+// Add a console log statement for the body of the function
+
+function sayHello() {
+    console.log("Hello!");
+}
+
+// Call the function
+
+sayHello();
+sayHello();
+sayHello();
+
+
+// Output
+
+/*
+
+Hello!
+Hello!
+Hello!
+
+=== Code Execution Successful ===
+
+*/
