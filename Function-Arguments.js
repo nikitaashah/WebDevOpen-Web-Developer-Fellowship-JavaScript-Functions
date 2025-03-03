287be5e386f052827fe72d8a893a012733df2435
Nikita Shah <nikita12.shah@gmail.com>
2025-03-03 20:43:01 +0530

    Example for arguments in functions.

---
+++ Function-Arguments.js
@@ -0,0 +1,23 @@
+// ====== Code ====== //
+
+
+function favouriteFood(food) {
+    console.log("My favourite food is " + food);
+}
+
+favouriteFood("pizza");
+
+
+// ==================================== //
+
+
+
+// ====== Output ====== //
+
+/*
+
+My favourite food is pizza
+
+=== Code Execution Successful ===
+
+*/
