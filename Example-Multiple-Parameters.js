15cb05c54468a1b541616269a0b4c100548e0022
Nikita Shah <nikita12.shah@gmail.com>
2025-03-04 23:24:51 +0530

    Example for multiple parameters in function.

---
+++ Example-Multiple-Parameters.js
@@ -0,0 +1,24 @@
+// ====== Code ====== //
+
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(2, 4);
+
+
+// ==================================== //
+
+
+
+// ====== Output ====== //
+
+
+/*
+
+6
+
+=== Code Execution Successful ===
+
+*/
